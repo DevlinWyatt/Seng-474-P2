9a0f4141032570337e8eb81560509f60534c1803
Devlin Wyatt <devlinwyatt@gmail.com>
2019-02-09 11:35:40 -0800

    Update a2p.py

--- a2p.py
+++ a2p.py
@@ -1,1 +1,34 @@
 #!/python3
+
+#Method used to Parse the file into a dictionary
+def parseFile(filename):
+	print("Parsing file")
+
+	f = open(filename)
+
+	N = int(f.readline().rstrip())
+	D = int(f.readline().rstrip())
+
+	#skip the header row
+	f.readline()
+
+	dic1 = {}
+
+	for i in range(N):
+		x = f.readline().rstrip().split('\t')
+		dic1[i] = {"label": x[0], "fs": []}
+
+		for j in range(D):
+			dic1[i]["fs"].append(float(x[j+1]))
+			
+	print("File parsed.")
+
+	return dic1
+	
+
+
+
+dic1 = {}
+
+dic1 = parseFile("data_10k_100.tsv")
+
