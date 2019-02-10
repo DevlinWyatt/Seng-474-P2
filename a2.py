8674ce296dbe16a04b175de67ae2d5e32a53e2bd
codebunny2020 <gr8can8dian@gmail.com>
2019-02-10 13:25:50 -0800

    Parse file changed. Data structure for feature set changed to dictionary.

--- a2.py
+++ a2.py
@@ -18,12 +18,15 @@ def parseFile(filename):
 	dic1 = {}
 	for i in range(N):
 		x = f.readline().rstrip().split('\t')
-		dic1[i] = {"label": x[0], "fs": []}
+		dic1[i] = {0: x[0], 1: {}}
 
 		for j in range(D):
-			dic1[i]["fs"].append(float(x[j+1]))
+			dic1[i][1].setdefault(j,[1,float(x[j+1])])
 	print("File parsed.")
 	return dic1
+
+def cost_function(feature_count, x, y):
+   return 0
 
 def question3_a(dictionaries,filename):
    # epoch_limit
@@ -36,35 +39,37 @@ def question3_a(dictionaries,filename):
    N = int(f.readline().rstrip())
    # Get number of features
    D = int(f.readline().rstrip())
+   f.close() #-------------------------------------
    for counter, item in enumerate(dictionaries):
-      #feature_list = np.array(dictionaries[counter]['fs'])
-      #feature_ones = np.ones((feature_list.shape[0]))
-      #feature_matrix = np.c_[feature_list,feature_ones]
       # Generate a matrix where each data point is a 2D feature vector
-      fcounter = 0
-      feature_matrix = []
-      for count, feature in enumerate(dictionaries[counter]['fs'],0):
-         #sys.stdout.write(str(count) + ' ')
-         # insert a column of 1's as the first entry in the feature vector
-         feature_matrix.append([feature,1])  
-     
-      weight_matrix=np.random.uniform(size=(len(feature_matrix)-1))
-      print(weight_matrix)
-      # loss_function = 1/(2*N)*(y-wt*x)
- 
-   # initialize weight matrix to have the same number of columns as 
-   # Loop until we reach T epochs OR loss is sufficently low OR
-      # Wgradient = evaluate_gradient(loss, data, W) using:
-         # loss function
-         # data
-         # weight matrix
-      # W += -1 * learning_rate * Wgradient 
+      Y = dictionaries[counter][0]
 
+      print(dictionaries[counter][1]) # feature_matrix with ones in first column 
+      # The feature set is a dictionary of int labels 0 to 99
+      # feature set dictionary contains a list [1, x-value]
+      # Access using dictionaries[counter][1]
+        
+  
+      # Get random weight for calculation  
+      # loss_function = 1/(2*N)*(y-wt*x) where N is the number of features.
+ 
+      # initialize weight matrix to have the same number of columns as 
+      # Loop until we reach T epochs OR loss is sufficently low OR
+         # Wgradient = evaluate_gradient(loss, data, W) using:
+            # loss function
+            # data
+            # weight matrix
+      
 
 filename = "data_10k_100.tsv"
 dic1 = parseFile(filename)
 question3_a(dic1,filename)
-
+#print(dic1[1])
 exit(0)
-#git clonegit clone https://github.com/DevlinWyatt/Seng-474-P2
+# git clonegit clone https://github.com/DevlinWyatt/Seng-474-P2
+# git init
+# git config user.name "someone"
+# git config user.email "someone@someplace.com"
+# git add *
+# git commit -m "some init msg"
 
